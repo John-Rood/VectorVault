035b2c194ffa91bbbbf3e63fa5c6bb9883a4582c
John-Rood <john@runner.one>
2023-06-07 23:57:44 -0500

    Update itemize.py

--- vectorvault/itemize.py
+++ vectorvault/itemize.py
@@ -1,4 +1,18 @@
-import datetime 
+# VECTOR VAULT CONFIDENTIAL
+# __________________
+# 
+#  All Rights Reserved.
+# 
+# NOTICE:  All information contained herein is, and remains
+# the property of Vector Vault and its suppliers,
+# if any.  The intellectual and technical concepts contained
+# herein are proprietary to Vector Vault
+# and its suppliers and may be covered by U.S. and Foreign Patents,
+# patents in process, and are protected by trade secret or copyright law.
+# Dissemination of this information or reproduction of this material
+# is strictly forbidden unless prior written permission is obtained
+# from Vector Vault. See license for consent.import datetime 
+
 from .vecreq import call_name_vecs, call_buildpath
 from annoy import AnnoyIndex
 import threading

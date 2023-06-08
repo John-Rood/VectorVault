396241cad4223318e3970a133f04fd948f9a1641
John-Rood <john@runner.one>
2023-06-07 23:56:26 -0500

    update

--- vectorvault/__init__.py
+++ vectorvault/__init__.py
@@ -1,19 +1,5 @@
-# VECTOR VAULT CONFIDENTIAL
-# __________________
-# 
-#  All Rights Reserved.
-# 
-# NOTICE:  All information contained herein is, and remains
-# the property of Vector Vault and its suppliers,
-# if any.  The intellectual and technical concepts contained
-# herein are proprietary to Vector Vault
-# and its suppliers and may be covered by U.S. and Foreign Patents,
-# patents in process, and are protected by trade secret or copyright law.
-# Dissemination of this information or reproduction of this material
-# is strictly forbidden unless prior written permission is obtained
-# from Vector Vault.
-
 from .vault import Vault
 from .download import download_url
 from .wrap import wrap
 from .signup import register
+from .tools_gpt import ToolsGPT

19bb3ab9f36e6b748d8b89e153e9d6498a69b24c
John-Rood <john@runner.one>
2023-06-09 15:06:50 -0500

    Client Access Update
    Added api key delete function. Protected user registration from overwriting any existing users. Added new api key generation function. Now can create video tutorials and not worry about api key exposure - tutorial videos for traffic generation to VV

--- vectorvault/__init__.py
+++ vectorvault/__init__.py
@@ -1,5 +1,5 @@
 from .vault import Vault
 from .download import download_url
 from .wrap import wrap
-from .signup import register
+from .cloud_api import register, get_new_key, delete_key
 from .tools_gpt import ToolsGPT

b9e070575da16c229ac28eac3d9c7e74992edb30
John-Rood <john@runner.one>
2023-08-06 12:46:16 -0500

    Custom Prompts do not need anything else to submit a chat response

--- vectorvault/ai.py
+++ vectorvault/ai.py
@@ -28,8 +28,9 @@ class AI:
                     history = history[-chars_left:]
                 else: # no history. If it was overlimit, then it was taken care of above
                     pass
+            prompt = prompt_template.format(content=user_input)
+
             if history:
-                prompt = prompt_template.format(content=user_input)
                 response = openai.ChatCompletion.create(
                     model=model,
                     messages=[
@@ -40,14 +41,13 @@ class AI:
             else:
                 # 'model' is the name of the model to use
                 # 'messages' is a list of message objects that mimics a conversation.
-                # Each object has a 'role' that can be 'system', 'user', or 'assistant', and a 'content' which is the actual content of the message.
-                prompt = prompt_template.format(content=user_input)
+                # Each object has a 'role' that can be either 'system', 'user', or 'assistant', and then 'content' is the actual content of the message.
                 response = openai.ChatCompletion.create(
                     model=model,
                     messages=[{"role": "user", "content": f"{prompt}"}]
                 )
                 return response['choices'][0]['message']['content']
-        else: # make no changes, and return response to passed in custom_prompt
+        else: # make no changes, and return response to custom_prompt
             if custom_prompt:
                 response = openai.ChatCompletion.create(
                     model=model,
@@ -58,7 +58,7 @@ class AI:
                 raise 'Error: Need custom_prompt if no user_input'
             
                     
-    def llm_w_context(self, user_input, context, history=None, model='gpt-3.5-turbo', max_tokens=4000, custom_prompt=False):
+    def llm_w_context(self, user_input = None, context = None, history=None, model='gpt-3.5-turbo', max_tokens=4000, custom_prompt=False):
         prompt_template = custom_prompt if custom_prompt else """
         Use the following Context to answer the Question at the end. 
         Answer as if you were the modern voice of the context, without referencing the context or mentioning that fact any context has been given. Make sure to not just repeat what is referenced. Don't preface or give any warnings at the end.
@@ -75,42 +75,44 @@ class AI:
         max_tokens = max_tokens * 4 if model == 'gpt-3.5-turbo-16k' else max_tokens
         max_tokens = max_tokens * 8 if model == 'gpt-4-32k' else max_tokens
 
-        intokes = self.get_tokens(user_input)
-        contokes = self.get_tokens(context)
-        history = history if history else ""
-        histokes = self.get_tokens(history)
-        promptokes = self.get_tokens(prompt_template)
+        if user_input and context:
+            intokes = self.get_tokens(user_input)
+            contokes = self.get_tokens(context)
+            history = history if history else ""
+            histokes = self.get_tokens(history)
+            promptokes = self.get_tokens(prompt_template)
 
-        if (intokes + contokes + histokes + promptokes) > max_tokens * .9:
-            tokes_left = max_tokens - intokes
-            if len(history) > 1:
-                tokes_left = (max_tokens/2) - intokes 
-                char_left = int(tokes_left * 4)
-                history = history[-char_left:]
-                tokes_left_after_hist = max_tokens - self.get_tokens(user_input + history)
-                char_left_after_hist = int(tokes_left_after_hist * 4)
-                context = context[-char_left_after_hist:]
-                double_check = self.get_tokens(user_input+history+context+prompt_template)
-                if double_check > max_tokens: 
-                    overby = double_check - max_tokens
-                    char_to_take_away = overby * 5
-                    context_length = len(context)
-                    remove_from_context = int(context_length - char_to_take_away)
-                    context = context[-remove_from_context:] 
-            else:
-                char_left = int(tokes_left * 4) 
-                context = context[-char_left:]
-                double_check = self.get_tokens(user_input + context)
-                if double_check > max_tokens:
-                    overby = double_check - max_tokens
-                    char_to_take_away = overby * 5
-                    context_length = len(context)
-                    remove_from_context = int(context_length - char_to_take_away)
-                    context = context[-remove_from_context:] 
+            if (intokes + contokes + histokes + promptokes) > max_tokens * .9:
+                tokes_left = max_tokens - intokes
+                if len(history) > 1:
+                    tokes_left = (max_tokens/2) - intokes 
+                    char_left = int(tokes_left * 4)
+                    history = history[-char_left:]
+                    tokes_left_after_hist = max_tokens - self.get_tokens(user_input + history)
+                    char_left_after_hist = int(tokes_left_after_hist * 4)
+                    context = context[-char_left_after_hist:]
+                    double_check = self.get_tokens(user_input+history+context+prompt_template)
+                    if double_check > max_tokens: 
+                        overby = double_check - max_tokens
+                        char_to_take_away = overby * 5
+                        context_length = len(context)
+                        remove_from_context = int(context_length - char_to_take_away)
+                        context = context[-remove_from_context:] 
+                else:
+                    char_left = int(tokes_left * 4) 
+                    context = context[-char_left:]
+                    double_check = self.get_tokens(user_input + context)
+                    if double_check > max_tokens:
+                        overby = double_check - max_tokens
+                        char_to_take_away = overby * 5
+                        context_length = len(context)
+                        remove_from_context = int(context_length - char_to_take_away)
+                        context = context[-remove_from_context:] 
 
-        # Format the prompt
-        user_input = history + user_input
-        prompt = prompt_template.format(context=context, history=history, content=user_input)
+            # Format the prompt
+            user_input = history + user_input
+            prompt = prompt_template.format(context=context, history=history, content=user_input)
+
         response = openai.ChatCompletion.create(
             model=model,
             messages=[
@@ -119,29 +121,31 @@ class AI:
         return response['choices'][0]['message']['content']
 
 
-    def llm_stream(self, user_input, history=None, model='gpt-3.5-turbo', max_tokens=4000, custom_prompt=False):
+    def llm_stream(self, user_input = None, history=None, model='gpt-3.5-turbo', max_tokens=4000, custom_prompt=False):
         '''
             Stream version of "llm"
         '''
         max_tokens = max_tokens * 4 if model == 'gpt-3.5-turbo-16k' else max_tokens
         max_tokens = max_tokens * 8 if model == 'gpt-4-32k' else max_tokens
         prompt_template = custom_prompt if custom_prompt else """{content}""" 
-        intokes = self.get_tokens(user_input)
-        histokes = self.get_tokens(history) if history else 0
-        if intokes + histokes > max_tokens:
-            tokes_left = max_tokens - intokes - histokes
-            if tokes_left < 0: # way too much input
-                char_to_remove = (tokes_left * -1) * 5 # make positive and remove that amount
-                user_input = user_input[char_to_remove:] # get in front of it, chop at max
-            if history:
-                intokes = self.get_tokens(user_input)
-                tokes_left = max_tokens - intokes
-                chars_left = int(tokes_left * 4)
-                history = history[-chars_left:]
-            else: # no history. If it was overlimit, then it was taken care of above
-                pass
-        if history:
+        
+        if user_input:
+            intokes = self.get_tokens(user_input)
+            histokes = self.get_tokens(history) if history else 0
+            if intokes + histokes > max_tokens:
+                tokes_left = max_tokens - intokes - histokes
+                if tokes_left < 0: # way too much input
+                    char_to_remove = (tokes_left * -1) * 5 # make positive and remove that amount
+                    user_input = user_input[char_to_remove:] # get in front of it, chop at max
+                if history:
+                    intokes = self.get_tokens(user_input)
+                    tokes_left = max_tokens - intokes
+                    chars_left = int(tokes_left * 4)
+                    history = history[-chars_left:]
+                else: # no history. If it was overlimit, then it was taken care of above
+                    pass
             prompt = prompt_template.format(content=user_input)
+        if history:
             response = openai.ChatCompletion.create(
                 model=model,
                 messages=[
@@ -156,7 +160,6 @@ class AI:
                         content = delta['content']
                         yield content
         else:
-            prompt = prompt_template.format(content=user_input)
             response = openai.ChatCompletion.create(
                 model=model,
                 messages=[{"role": "user", "content": f"{prompt}"}],
@@ -171,7 +174,7 @@ class AI:
                         yield content
                         
                     
-    def llm_w_context_stream(self, user_input, context, history=None, model='gpt-3.5-turbo', max_tokens=4000, custom_prompt=False):
+    def llm_w_context_stream(self, user_input = None, context = None, history=None, model='gpt-3.5-turbo', max_tokens=4000, custom_prompt=False):
         prompt_template = custom_prompt if custom_prompt else """
         Use the following Context to answer the Question at the end. 
         Answer as if you were the modern voice of the context, without referencing the context or mentioning that fact any context has been given. Make sure to not just repeat what is referenced. Don't preface or give any warnings at the end.
@@ -188,41 +191,42 @@ class AI:
         max_tokens = max_tokens * 4 if model == 'gpt-3.5-turbo-16k' else max_tokens
         max_tokens = max_tokens * 8 if model == 'gpt-4-32k' else max_tokens
 
-        intokes = self.get_tokens(user_input)
-        contokes = self.get_tokens(context)
-        history = history if history else ""
-        histokes = self.get_tokens(history)
-        promptokes = self.get_tokens(prompt_template)
+        if user_input:
+            intokes = self.get_tokens(user_input)
+            contokes = self.get_tokens(context)
+            history = history if history else ""
+            histokes = self.get_tokens(history)
+            promptokes = self.get_tokens(prompt_template)
+            if (intokes + contokes + histokes + promptokes) > max_tokens * .9:
+                tokes_left = max_tokens - intokes
+                if len(history) > 1:
+                    tokes_left = (max_tokens/2) - intokes 
+                    char_left = int(tokes_left * 4)
+                    history = history[-char_left:]
+                    tokes_left_after_hist = max_tokens - self.get_tokens(user_input + history)
+                    char_left_after_hist = int(tokes_left_after_hist * 4)
+                    context = context[-char_left_after_hist:]
+                    double_check = self.get_tokens(user_input+history+context+prompt_template)
+                    if double_check > max_tokens: 
+                        overby = double_check - max_tokens
+                        char_to_take_away = overby * 5
+                        context_length = len(context)
+                        remove_from_context = int(context_length - char_to_take_away)
+                        context = context[-remove_from_context:] 
+                else:
+                    char_left = int(tokes_left * 4) 
+                    context = context[-char_left:]
+                    double_check = self.get_tokens(user_input + context)
+                    if double_check > max_tokens:
+                        overby = double_check - max_tokens
+                        char_to_take_away = overby * 5
+                        context_length = len(context)
+                        remove_from_context = int(context_length - char_to_take_away)
+                        context = context[-remove_from_context:] 
 
-        if (intokes + contokes + histokes + promptokes) > max_tokens * .9:
-            tokes_left = max_tokens - intokes
-            if len(history) > 1:
-                tokes_left = (max_tokens/2) - intokes 
-                char_left = int(tokes_left * 4)
-                history = history[-char_left:]
-                tokes_left_after_hist = max_tokens - self.get_tokens(user_input + history)
-                char_left_after_hist = int(tokes_left_after_hist * 4)
-                context = context[-char_left_after_hist:]
-                double_check = self.get_tokens(user_input+history+context+prompt_template)
-                if double_check > max_tokens: 
-                    overby = double_check - max_tokens
-                    char_to_take_away = overby * 5
-                    context_length = len(context)
-                    remove_from_context = int(context_length - char_to_take_away)
-                    context = context[-remove_from_context:] 
-            else:
-                char_left = int(tokes_left * 4) 
-                context = context[-char_left:]
-                double_check = self.get_tokens(user_input + context)
-                if double_check > max_tokens:
-                    overby = double_check - max_tokens
-                    char_to_take_away = overby * 5
-                    context_length = len(context)
-                    remove_from_context = int(context_length - char_to_take_away)
-                    context = context[-remove_from_context:] 
-
-        prompt = prompt_template.format(context=context, history=history, content=user_input)
-        user_input = history + user_input
+            user_input = history + user_input
+            prompt = prompt_template.format(context=context, history=history, content=user_input)
+        
         response = openai.ChatCompletion.create(
             model=model,
             messages=[
